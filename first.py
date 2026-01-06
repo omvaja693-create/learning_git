811de75b9e988598aa0380f95242ec2f4d14083a
Om Vaja <omvaja693.com>
2026-01-06 04:14:21 -1200

    practice

---
+++ first.py
@@ -0,0 +1,80 @@
+#print('om','vaja' , sep = "-", end = "+")
+'''
+a = input("Enter Your name:")
+b = int(input("Enter Your Age:"))
+c = input("Enter Your Hobby:")
+
+print('Hello',a,'At',b,'enjoying',c,'sound`s fun')
+
+
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+
+print ('Addition of',a,'and',b,'is',a+b)
+
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+
+print ('Substraction of',a,'and',b,'is',a-b)
+
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+
+print ('Multiplication of',a,'and',b,'is',a*b)
+
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+
+print ('Division of',a,'and',b,'is',a/b)
+
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+
+c = a**b
+print ('percentage of',a,'and',b,'is',c)
+
+
+int = a = 5
+float = b =5.5
+str = 'om'
+ 
+
+print(a,b,str,sep = "-")
+print(type(a))
+
+
+
+a = float(input('Enter your Height:'))
+b = int(input('Enter your Weight:'))
+
+print('Your Height is',a, 'cm','and Weight is',b, 'kg')
+
+
+
+a = 5
+a //=2
+
+print(a)
+
+
+'''
+
+name = 'om'
+print (name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
